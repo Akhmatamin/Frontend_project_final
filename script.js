1c0e487c5ca918fb8ef08a58add7b85194b8408c
Akhmatamin <ahmatamin.raimzhanov@alatoo.edu.kg>
2025-02-17 14:38:23 +0600

    Acces_question

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
-    let answer = prompt("Are you Alatoshnik?");
-
-    if (answer === null || answer.trim() === "" || answer==="No" || answer==="no" || answer==="нет") {
-        alert("You are not allowed!")
-        window.location.href = "https://www.google.com"; 
+function checkAnswer(answer) {
+    if (answer === "No") {
+        alert("You are not allowed!");
+        window.location.href = "https://www.google.com";
     } else {
-        alert("You are welcome!!");
+        alert("Welcome, you are allowed!");
+        document.getElementById('check').style.display = 'none'; 
+        document.getElementById('content').style.display = 'block'; 
     }
+}
 
+function showNewsAlert() {
+    alert('No News yet!');
+}
 
+function showResultsAlert() {
+    alert('No Results yet!');
+}
+
+function showTablesAlert() {
+    alert('No tables yet!');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('newsLink').addEventListener('click', showNewsAlert);
+    document.getElementById('resultsLink').addEventListener('click', showResultsAlert);
+    document.getElementById('tablesLink').addEventListener('click', showTablesAlert);
+});
